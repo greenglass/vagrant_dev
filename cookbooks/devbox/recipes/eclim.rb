e31129b0bdeedf864e880563480e8803f7db1f52
Derek Mardian <greenglasscode@gmail.com>
2016-01-03 10:30:20 -0800

    Uncommented downlaod of eclim jar.

--- cookbooks/devbox/recipes/eclim.rb
+++ cookbooks/devbox/recipes/eclim.rb
@@ -1,9 +1,9 @@
 node[:users].each do |u|
-#  remote_file "/tmp/eclim_2.5.0.jar" do
-#    source 'http://sourceforge.net/projects/eclim/files/eclim/2.5.0/eclim_2.5.0.jar/download'  
-#    owner u
-#    group u
-#  end
+  remote_file "/tmp/eclim_2.5.0.jar" do
+    source 'http://sourceforge.net/projects/eclim/files/eclim/2.5.0/eclim_2.5.0.jar/download'  
+    owner u
+    group u
+  end
 
   home = "/home/#{u}"
   execute "Install eclim for #{u}" do
